b5d51168d3ea2bd73119136a2fef34effe7b5d4d
Shoaib Naseer <43854873+Shoaib-Naseer@users.noreply.github.com>
2023-03-19 13:48:02 +0500

    Added Mobile Navigation

---
+++ main.js
@@ -0,0 +1,8 @@
+window.onload = function digital_fn() {
+  const toggle = document.querySelector('.toggle-btn');
+  const collapse = document.querySelector('.collapse');
+
+  toggle.addEventListener('click', function () {
+    collapse.classList.toggle('active');
+  });
+};
